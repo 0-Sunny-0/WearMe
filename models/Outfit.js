eee02dd93d01be261af8926ad703a5f5b9de9325
Sunny Le Bunz <msafinch@gmail.com>
2024-07-23 19:10:19 -0400

    datatype change

--- models/Outfit.js
+++ models/Outfit.js
@@ -12,7 +12,7 @@ Outfit.init(
       autoIncrement: true
     },
     items: {
-      type: DataTypes.ARRAY(DataTypes.STRING),
+      type: DataTypes.ARRAY(DataTypes.INTEGER),
       allowNull: false
     },
     username: {
