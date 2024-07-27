6256b057e0174c3cf81c1a9d4bfc495e1139e075
Michael Medina <michaeljmedina22800@gmail.com>
2024-07-27 17:26:54 -0400

    fixed api routes

--- routes/api/index.js
+++ routes/api/index.js
@@ -1,11 +1,9 @@
 const router = require('express').Router();
-const closetCategories = require('./closet');
-const clothingSlots = require('./clothing-slots');
+const closets = require('./closets');
 const outfitItems = require('./outfit-items');
 const outfits = require('./outfits');
 
-router.use('/closet-categories', closetCategories);
-router.use('/clothing-slots', clothingSlots);
+router.use('/closets', closets);
 router.use('/outfit-items', outfitItems);
 router.use('/outfits', outfits);
 
